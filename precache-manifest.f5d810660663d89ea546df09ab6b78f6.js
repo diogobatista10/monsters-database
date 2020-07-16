b1f021884063a88a6d4fb7048fa3b0be8683f4ae
Diogo Pinto Batista <diogo.pinto.batista@blip.pt>
2020-07-16 14:18:53 +0100

    Updates

---
+++ precache-manifest.f5d810660663d89ea546df09ab6b78f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ab00961156a83e9c5a7b708be5d91a",
+    "url": "/monsters-database/index.html"
+  },
+  {
+    "revision": "d1fa66080c73f8d0dc36",
+    "url": "/monsters-database/static/css/main.04161a6e.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-database/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-database/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1fa66080c73f8d0dc36",
+    "url": "/monsters-database/static/js/main.82c65b99.chunk.js"
+  },
+  {
+    "revision": "143238c1f6d94e48ff24",
+    "url": "/monsters-database/static/js/runtime-main.14a168f2.js"
+  }
+]);
